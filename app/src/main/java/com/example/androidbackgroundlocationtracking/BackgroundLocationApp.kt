2945693ddbb1a6952f3d4bb21d96ea3f64db6d20
Amitav Singh <amitavsingh@30HW012026.Ian>
2022-09-29 14:29:25 +0200

    Initial Commit - Added Geo Location Permissions and Handling Geo Location Service.

---
+++ app/src/main/java/com/example/androidbackgroundlocationtracking/BackgroundLocationApp.kt
@@ -0,0 +1,10 @@
+package com.example.androidbackgroundlocationtracking
+
+import android.app.Application
+
+class BackgroundLocationApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
